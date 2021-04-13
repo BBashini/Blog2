99faf697ad3ff12f187fb7bbfcd4151f550cc5b5
BBashini <b4balach@ryerson.ca>
2021-04-13 06:15:14 -0400

    button and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from 'react'
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Router>
+      <Navbar/>
+      
+      <Switch>
+        <Route path='/' exact></Route>
+      </Switch>
+      
+    </Router>
     <h1>Kobi I am marrying you</h1>
-    </div>
+    </>
   );
 }
 
